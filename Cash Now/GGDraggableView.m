92ab3502439bdbd9e2f8c64e2b04b72db570af73
asoviche <amaury.soviche@icloud.com>
2015-02-28 11:31:55 +0100

    done

--- Cash Now/GGDraggableView.m
+++ Cash Now/GGDraggableView.m
@@ -32,6 +32,18 @@
     self = [super initWithFrame:frame];
     if (!self) return nil;
     
+    //1. load xib file
+    [[NSBundle mainBundle] loadNibNamed:@"ViewJob" owner:self options:nil];
+    
+    //2. adjust bounds
+    self.bounds = self.viewJob.bounds;
+    
+    //3. add as a subview
+    [self addSubview:self.viewJob];
+    
+    
+    
+    
 
     [self setBackgroundColor:[UIColor whiteColor]];
     
@@ -54,72 +66,72 @@
     self.layer.borderColor = [UIColorFromRGB(0xD1D1D1) CGColor];
     
     //labels
-    self.LabelDescriptionJob = [[UITextView alloc]init];
-    [self addSubview:self.LabelDescriptionJob];
-    self.LabelDescriptionJob.editable=NO;
-    [self.LabelDescriptionJob setFont:[UIFont fontWithName:@"HelveticaNeue" size:16]];
-    self.LabelDescriptionJob.frame = CGRectMake(10, 255, 270,100);
-    self.LabelDescriptionJob.textColor = UIColorFromRGB(0x225378);
-
-    
-    self.LabelDateJob = [[UILabel alloc]init];
-    [self addSubview:self.LabelDateJob];
-    [self.LabelDateJob setFont:[UIFont fontWithName:@"HelveticaNeue" size:11]];
-    self.LabelDateJob.frame = CGRectMake(95, 430, 300,15);
-    self.LabelDateJob.textColor = UIColorFromRGB(0x225378);
-    
-    self.LabelPriceJob = [[UILabel alloc]init];
-    [self addSubview:self.LabelPriceJob];
-    [self.LabelPriceJob setFont:[UIFont fontWithName:@"HelveticaNeue" size:11]];
-    self.LabelPriceJob.frame = CGRectMake(227, 430, 290,15);
-    self.LabelPriceJob.textColor = UIColorFromRGB(0x225378);
-    
-    self.LabelHourJob = [[UILabel alloc]init];
-    [self addSubview:self.LabelHourJob];
-    [self.LabelHourJob setFont:[UIFont fontWithName:@"HelveticaNeue" size:11]];
-    self.LabelHourJob.frame = CGRectMake(160, 430, 300,15);
-    self.LabelHourJob.textColor = UIColorFromRGB(0x225378);
-    
-    self.DistanceToUser = [[UILabel alloc]init];
-    [self addSubview:self.DistanceToUser];
-    [self.DistanceToUser setFont:[UIFont fontWithName:@"HelveticaNeue" size:11]];
-    self.DistanceToUser.frame = CGRectMake(30, 430, 300,15);
-    self.DistanceToUser.textColor = UIColorFromRGB(0x225378);
+//    self.LabelDescriptionJob = [[UITextView alloc]init];
+//    [self addSubview:self.LabelDescriptionJob];
+//    self.LabelDescriptionJob.editable=NO;
+//    [self.LabelDescriptionJob setFont:[UIFont fontWithName:@"HelveticaNeue" size:16]];
+//    self.LabelDescriptionJob.frame = CGRectMake(10, 255, 270,100);
+//    self.LabelDescriptionJob.textColor = UIColorFromRGB(0x225378);
+//
+//    
+//    self.LabelDateJob = [[UILabel alloc]init];
+//    [self addSubview:self.LabelDateJob];
+//    [self.LabelDateJob setFont:[UIFont fontWithName:@"HelveticaNeue" size:11]];
+//    self.LabelDateJob.frame = CGRectMake(95, 430, 300,15);
+//    self.LabelDateJob.textColor = UIColorFromRGB(0x225378);
+//    
+//    self.LabelPriceJob = [[UILabel alloc]init];
+//    [self addSubview:self.LabelPriceJob];
+//    [self.LabelPriceJob setFont:[UIFont fontWithName:@"HelveticaNeue" size:11]];
+//    self.LabelPriceJob.frame = CGRectMake(227, 430, 290,15);
+//    self.LabelPriceJob.textColor = UIColorFromRGB(0x225378);
+//    
+//    self.LabelHourJob = [[UILabel alloc]init];
+//    [self addSubview:self.LabelHourJob];
+//    [self.LabelHourJob setFont:[UIFont fontWithName:@"HelveticaNeue" size:11]];
+//    self.LabelHourJob.frame = CGRectMake(160, 430, 300,15);
+//    self.LabelHourJob.textColor = UIColorFromRGB(0x225378);
+//    
+//    self.DistanceToUser = [[UILabel alloc]init];
+//    [self addSubview:self.DistanceToUser];
+//    [self.DistanceToUser setFont:[UIFont fontWithName:@"HelveticaNeue" size:11]];
+//    self.DistanceToUser.frame = CGRectMake(30, 430, 300,15);
+//    self.DistanceToUser.textColor = UIColorFromRGB(0x225378);
     
     
     //ICONS
     
-    UIImageView *iconLoc = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"icon-location"]];
-    [self addSubview:iconLoc];
-    iconLoc.contentMode = UIViewContentModeScaleAspectFit;
-    iconLoc.frame = CGRectMake(30, 390, 30,35);
-    
-    UIImageView *iconCal = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"icon-calendar"]];
-    [self addSubview:iconCal];
-    iconCal.contentMode = UIViewContentModeScaleAspectFit;
-    iconCal.frame = CGRectMake(95, 390, 28,31);
-    
-    UIImageView *iconClock = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"icon-clock"]];
-    [self addSubview:iconClock];
-    iconClock.contentMode = UIViewContentModeScaleAspectFit;
-    iconClock.frame = CGRectMake(160, 390, 30,30);
-    
-    UIImageView *iconDollar = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"icon-dollar"]];
-    [self addSubview:iconDollar];
-    iconDollar.contentMode = UIViewContentModeScaleAspectFit;
-    iconDollar.frame = CGRectMake(225, 390, 30,30);
+//    UIImageView *iconLoc = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"icon-location"]];
+//    [self addSubview:iconLoc];
+//    iconLoc.contentMode = UIViewContentModeScaleAspectFit;
+//    iconLoc.frame = CGRectMake(30, 390, 30,35);
+//    
+//    UIImageView *iconCal = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"icon-calendar"]];
+//    [self addSubview:iconCal];
+//    iconCal.contentMode = UIViewContentModeScaleAspectFit;
+//    iconCal.frame = CGRectMake(95, 390, 28,31);
+//    
+//    UIImageView *iconClock = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"icon-clock"]];
+//    [self addSubview:iconClock];
+//    iconClock.contentMode = UIViewContentModeScaleAspectFit;
+//    iconClock.frame = CGRectMake(160, 390, 30,30);
+//    
+//    UIImageView *iconDollar = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"icon-dollar"]];
+//    [self addSubview:iconDollar];
+//    iconDollar.contentMode = UIViewContentModeScaleAspectFit;
+//    iconDollar.frame = CGRectMake(225, 390, 30,30);
     
     
     //lines
-    UIImageView *line2 = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"line_grey"]];
-    [self addSubview:line2];
-    line2.contentMode = UIViewContentModeScaleAspectFit;
-    line2.frame = CGRectMake(80, 240, 130,5);
-    
-    UIImageView *line1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"line_grey"]];
-    [self addSubview:line1];
-    line1.contentMode = UIViewContentModeScaleAspectFit;
-    line1.frame = CGRectMake(80, 350, 130,30);
+//    UIImageView *line2 = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"line_grey"]];
+//    [self addSubview:line2];
+//    line2.contentMode = UIViewContentModeScaleAspectFit;
+//    line2.frame = CGRectMake(80, 240, 130,5);
+//    
+//    UIImageView *line1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"line_grey"]];
+//    [self addSubview:line1];
+//    line1.contentMode = UIViewContentModeScaleAspectFit;
+//    line1.frame = CGRectMake(80, 350, 130,30);
     
     //launch loading
     self.activity = [[UIActivityIndicatorView alloc]init];
@@ -133,22 +145,22 @@
 
 -(void) tapped: (UITapGestureRecognizer *) tapGestureRecognizer {
     
-    [self.delegate GGDraggableViewDelegate_LoadDetailView];
+//    [self.delegate GGDraggableViewDelegate_LoadDetailView];
 }
 
-- (void)loadImageAndStyle : (UIImage *) imageJob
-{
-    UIImageView *imageView = [[UIImageView alloc] initWithImage:imageJob];
-    imageView.clipsToBounds = YES;
-    [self addSubview:imageView];
-    imageView.frame = CGRectMake(0, 0, super.frame.size.width, 230);
-    imageView.contentMode = UIViewContentModeScaleAspectFit;
-    
-    imageView.clipsToBounds=YES;
-    imageView.layer.cornerRadius=3;
-    
- 
-}
+//- (void)loadImageAndStyle : (UIImage *) imageJob
+//{
+//    UIImageView *imageView = [[UIImageView alloc] initWithImage:imageJob];
+//    imageView.clipsToBounds = YES;
+//    [self addSubview:imageView];
+//    imageView.frame = CGRectMake(0, 0, super.frame.size.width, 230);
+//    imageView.contentMode = UIViewContentModeScaleAspectFit;
+//    
+//    imageView.clipsToBounds=YES;
+//    imageView.layer.cornerRadius=3;
+//    
+// 
+//}
 
 
 - (void)dragged:(UIPanGestureRecognizer *)gestureRecognizer
